975f70c74be8518508a0b03edd1dd3c306e8a152
Oexi <oexi@users.noreply.github.com>
2026-08-18 11:14:01 +0000

    chore: 版本号升至 3.2.2(import 迁移至 V3 SDK 的补丁版本)

--- plugins.v3/jackettextend/__init__.py
+++ plugins.v3/jackettextend/__init__.py
@@ -48,7 +48,7 @@ class JackettExtend(_PluginBase):
     # 插件图标
     plugin_icon = "Jackett_A.png"
     # 插件版本
-    plugin_version = "3.2.1"
+    plugin_version = "3.2.2"
     # 插件作者
     plugin_author = "jtcymc"
     # 作者主页
